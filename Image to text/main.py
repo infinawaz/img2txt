477fdcdf5fd93a8b8ad3646efc4e0f81953a2890
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2025-02-15 23:08:51 +0530

    removed comments and updated path

--- Image to text/main.py
+++ Image to text/main.py
@@ -1,22 +1,15 @@
 from translate import Translator
-
 from PIL import Image
 import pytesseract
 
-# Set the path to the tesseract executable (update the path as per your system)
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-# Load the image
-image_path = 'E:/Final Yr Project/img2txt/Image to text/Images/image.png'  # Replace with your image path
-image = Image.open(image_path)
+image_path = 'E:\img2txt\Image to text\Images\image.png'
 
-# Extract text from the image
-extracted_text = pytesseract.image_to_string(image)
+extracted_text = pytesseract.image_to_string(image_path)
 
-# Print the extracted text
 print("Extracted Text:\n", extracted_text)
 
-# Translate the extracted text
 translator= Translator(to_lang="hi")
 translation = translator.translate(extracted_text)
 print("Translated Text:", translation)
